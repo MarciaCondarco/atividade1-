daf6c93dbc92fad3578cd86b1090cd24683145dc
MarciaCondarco <142843705+MarciaCondarco@users.noreply.github.com>
2024-05-24 09:16:55 -0300

    atividade de converter celsius para fahrenheit

---
+++ atividade3.js
@@ -0,0 +1,4 @@
+var temperaturaDeCelsius=36
+var temperaturaDeFahrenheit=temperaturaDeCelsius*(9/5)+32
+
+console.log("a temperatura de celsius:", temperaturaDeCelsius, "para fahrenheit é:", temperaturaDeFahrenheit)
