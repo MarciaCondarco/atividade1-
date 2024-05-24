c12550ad475cc5e146a94dbf3e0b755a1c03bf9f
MarciaCondarco <142843705+MarciaCondarco@users.noreply.github.com>
2024-05-24 09:18:01 -0300

    preço final do produto no desconto de 10%

---
+++ atividade5.js
@@ -0,0 +1,6 @@
+var valorDoProduto = 100
+var valorDoDesconto=10
+var valorDaDiferencaComDesconto=(valorDoProduto*10)/100
+var valorFinalDoProduto= valorDoProduto-valorDaDiferencaComDesconto
+
+console.log("o valor do desconto é ", valorDoDesconto, "no preço do produto ",valorDoProduto, "o preço final com o desconto é ", valorFinalDoProduto)
