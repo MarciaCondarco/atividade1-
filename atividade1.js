930676f4c78291415bb44d02c0ecb2cf8395d936
MarciaCondarco <142843705+MarciaCondarco@users.noreply.github.com>
2024-05-24 09:14:33 -0300

    atividade 1 - operações básicas com duas váriaveis

---
+++ atividade1.js
@@ -0,0 +1,5 @@
+var primeiraVariavel = 3
+var segundaVariavel = 2
+var resultadoDaSoma = primeiraVariavel + segundaVariavel
+
+console.log("a soma de", primeiraVariavel, "+", segundaVariavel, ":", resultadoDaSoma)
